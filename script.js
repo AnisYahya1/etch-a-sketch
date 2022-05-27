38cb49997cdef35759d74b201f20569071b37f63
AnisYahya <anisy309@gmail.com>
2022-05-27 18:23:39 -0500

    Creating A 16x16 Grid

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 const grid = document.querySelector('.grid-square');
 
 
-for (let i = 0; i < (16); i++) {
+for (let i = 0; i < 16; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
     row.style.cssText = 'display: flex; flex-wrap: wrap;';  
     for (let i = 0; i < 16; i++) {
-        const col = document.createElement('div')
+        const col = document.createElement('div');
         col.classList.add('col');
-        col.style.cssText = 'flex-basis: 100%;';
-        col.textContent = "a";
+        col.style.cssText = 'flex-basis: 100%; background-color: white;';
         row.appendChild(col);
     }
     grid.appendChild(row);
-    
 }
